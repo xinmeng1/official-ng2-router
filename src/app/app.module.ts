b5fa048d27e09154d9f2486df8b8a5c4efaa6dc7
Xin Meng <mengxin.city@gmail.com>
2016-12-14 22:45:28 +0000

    Milestone #1: Getting Started with the Router: basic router in the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {CrisisListComponent} from './crisis-list.component';
+import {HeroListComponent} from './hero-list.component';
+
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent,
+    CrisisListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
